e33251fc7762268a64dbb4bef9ceaf434f7c7115
Aditya Singh <adityasingh740745@gmail.com>
2024-09-28 13:53:14 +0530

    learned about using the basic useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import Card from "./Components/Card";
+import React, { useState } from "react";
+// import Card from "./Components/Card";
 
 function App() {
+    const [score, setScore] = useState(0);
     return (
-        <>
-            <Card />
-        </>
+        <div className="p-10">
+            <h1 className="text-xl font-semibold">Score: {score}</h1>
+            <button onClick={() => setScore(score+10)} className="px-2 py-1 bg-blue-500 text-white rounded mt-5">
+                Add More
+            </button>
+        </div>
     );
 }
 
