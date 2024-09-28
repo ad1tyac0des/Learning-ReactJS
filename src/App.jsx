a878926bb18df5db907674b317a99d51e46cad8d
Aditya Singh <adityasingh740745@gmail.com>
2024-09-28 14:24:31 +0530

    learned how to get the previous val inside setVal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
+
 function App() {
-    const [banned, setBanned] = useState(false)
+    const [val, setVal] = useState(0)
 
     return (
         <div className='p-10'>
-            <h1 className={`text-lg font-semibold ${banned ? 'text-red-500' : 'text-green-500'}`}>{banned ? 'Banned' : 'Not Banned'}</h1>
-            <button onClick={()=> setBanned(!banned)} className={`px-6 py-1 text-white rounded-full text-lg mt-10 bg-red-500`}>Ban</button>
+            <h1 className='text-xl font-semibold'>
+                {val}
+            </h1>
+
+            <button onClick={() => setVal(prev => prev + 1)} className='px-4 py-1 bg-sky-500 text-white rounded'>Add</button>
         </div>
     )
 }
