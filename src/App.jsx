cdebc928214f0b3977a499fa2f2473efea713865
Aditya Singh <adityasingh740745@gmail.com>
2024-09-28 14:44:29 +0530

    learned to update values of reference types in useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
 
 function App() {
-    const [val, setVal] = useState(0)
+    const [val, setVal] = useState({"name": "harsh", "isBanned": false})
 
     return (
         <div className='p-10'>
-            <h1 className='text-xl font-semibold'>
-                {val}
-            </h1>
-
-            <button onClick={() => setVal(prev => prev + 1)} className='px-4 py-1 bg-sky-500 text-white rounded'>Add</button>
+            <h1 className='text-lg font-semibold'>Name: {val.name}</h1>
+            <h1 className='text-lg font-semibold'>Banned: {val.isBanned.toString()}</h1>
+            <button onClick={()=> setVal({...val, isBanned: !val.isBanned})} className='px-3 py-1 bg-blue-500 text-white uppercase font-bold rounded mt-4'>Change</button>
         </div>
     )
 }
