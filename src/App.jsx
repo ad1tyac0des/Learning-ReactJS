a1ba6dfef43cb3179518c65286fccac2d7931584
Aditya Singh <adityasingh740745@gmail.com>
2024-10-01 23:50:24 +0530

    change obj value inside array

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
 
 function App() {
-    const [val, setVal] = useState([1, 2, 3, 4, 5, 6]);
+    const [val, setVal] = useState([
+        { name: "Aditya", age: 19 },
+        { name: "Harsh", age: 34 },
+        { name: "Rohan", age: 30 },
+    ]);
     return (
         <div className="p-10">
-            <button onClick={()=> setVal(val.filter(elem => elem%2 !== 0))} className="px-3 py-1 bg-blue-500 rounded mb-5 text-white font-semibold">
-                Remove
+            <button
+                onClick={() =>
+                    setVal(
+                        val.map((elem) =>
+                            elem.name == "Harsh" ? { name: "Harsh", age: 25 } : elem
+                        )
+                    )
+                }
+                className="px-3 py-1 bg-blue-500 rounded mb-5 text-white font-semibold"
+            >
+                Add
             </button>
+
             {val.map((elem, index) => (
-                <h1 key={index}>{elem}</h1>
+                <div key={index}>
+                    <h1>{elem.name}</h1>
+                    <h1>{elem.age}</h1>
+                </div>
             ))}
         </div>
     );
