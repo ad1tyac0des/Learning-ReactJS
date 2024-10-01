db57f9e97f97a8a6e3a85ffe3fde02d288470f58
Aditya Singh <adityasingh740745@gmail.com>
2024-10-01 23:38:29 +0530

    removing the even numbers in array using setVal

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,11 @@ function App() {
     const [val, setVal] = useState([1, 2, 3, 4, 5, 6]);
     return (
         <div className="p-10">
-            <button
-                onClick={() =>
-                    setVal(val.filter((item, index) => index !== val.length - 1))
-                }
-                className="px-3 py-1 bg-blue-500 rounded mb-5 text-white font-semibold"
-            >
-                Remove Last Element
+            <button onClick={()=> setVal(val.filter(elem => elem%2 !== 0))} className="px-3 py-1 bg-blue-500 rounded mb-5 text-white font-semibold">
+                Remove
             </button>
             {val.map((elem, index) => (
-                <h1 className="font-semibold" key={index}>
-                    {elem}
-                </h1>
+                <h1 key={index}>{elem}</h1>
             ))}
         </div>
     );
