e67d59f62c081fdbe46b598795fa5428d370f47b
Aditya Singh <adityasingh740745@gmail.com>
2024-10-06 20:44:15 +0530

    handling form using 'useRef'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import styles from './Components/style.module.css'
+import React, { useRef } from 'react'
 
 function App() {
+
+    const name = useRef(null);
+    const age = useRef(null);
+
+    const handleSubmit = (event)=> {
+        event.preventDefault()
+        console.log(name.current.value)
+        console.log(age.current.value)
+    }
+
     return (
-        <div>
-            <h1 className={`${styles.a} ${styles.b}`}>App</h1>
-        </div>
+        <form className='p-4 flex gap-5' action="" onSubmit={handleSubmit}>
+            <input ref={name} className='border border-zinc-700 outline-none px-2' placeholder='Name' type="text" />
+            <input ref={age} className='border border-zinc-700 outline-none px-2' placeholder='Age' type="text" />
+            <input className='px-5 border border-zinc-700 cursor-pointer' type="submit" value="Submit" />
+        </form>
     )
 }
 
